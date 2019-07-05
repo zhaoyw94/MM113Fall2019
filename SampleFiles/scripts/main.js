184d73c2291307b6d149ff5f703175fc1931fc3a
ireichert <ireichert@ohlone.edu>
2019-07-04 20:20:09 -0700

    Update main.js

--- SampleFiles/scripts/main.js
+++ SampleFiles/scripts/main.js
@@ -10,10 +10,6 @@ const modifyText = () => {
   //Change the text of message2 to be the message of the StarTrekTweet object
   //Change the text of user2 to be the handle from the StarTrekTweet object
   //Changes the text of message 2 class to be the handle from the StarTrekTweet object
-  document.querySelector('#message2').innerHTML = StarTrekTweet.name
-  document.getElementById('user2').innerHTML = StarTrekTweet.handle
-  document.getElementById('message2').innerHTML = StarTrekTweet.text
-  //console.log('modifying text')
   // YOUR CODE BELOW THIS LINE.
 }
 
@@ -21,9 +17,7 @@ const changeLinkAndImageAttributes = () => {
   //Set the href attribute for link1 to point to the first element in the links array
   //Sets the href attribute for link2 to point to the second element in the links array
   //Sets the src attribute for the Ohlone image to point to the third element in the links array
-  document.querySelector('#link1').href = links[0];
-  document.querySelector('#link2').href = links[1];
-  document.querySelector('#Ohlone').src = links[2];
+  // YOUR CODE BELOW THIS LINE.
 }
 
 const changeClassAttribute = () => {
@@ -31,8 +25,7 @@ const changeClassAttribute = () => {
   //function so that it replaces the “tweet” class with the “tweet-pink” class for the two tweets.
   // change id="tweet1" class="tweet" to id="tweet1" class="tweet-pink"
   // YOUR CODE BELOW THIS LINE
-  document.querySelector('#tweet1').className = 'tweet-pink'
-  //  document.getElementById('tweet1').className = 'tweet-pink'
+  
 }
 
 const appendElements = () => {
@@ -41,11 +34,6 @@ const appendElements = () => {
   //B. An "img" element (an image) to the bottom of tweet1 that
   //points to an image of your choice,symbolizing something about the course.
   //id="tweet1"
-  const NewLink = document.createElement('a')
-  NewLink.href = "http://www.ohlone.edu"
-  NewLink.appendChild(document.createTextNode("intro to JavaScript"))
-  document.getElementById('tweet1').appendChild(NewLink)
-
   // YOUR CODE BELOW THIS LINE
 
 }
